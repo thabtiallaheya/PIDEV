f023419a95036486d8c9e8cae50c086385c73723
AmdouniBrahmiKhouloud <khouloud.amdounibrahmi@esprit.tn>
2022-03-07 21:54:06 +0100

    adding nodemon

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,21 @@
 const express = require('express');
 const app = express();
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
+
 app.use(express.json())
-mongoose.connect("mongodb+srv://Firas:qs2DNSfxzNVgWzT5@pidev.rwjs1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", {useNewUrlParser: true })
-app.listen(8000, ()=> {
-    console.log('app is running in port 8000')
-})  
+mongoose.connect(
+    "mongodb+srv://Firas:qs2DNSfxzNVgWzT5@pidev.rwjs1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }).then(() => console.log('MongoDB connected!'))
+    .catch(err => console.log(err));
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+app.listen(3001, () => {
+    console.log('app is running in port 3001');
+})
+
+
+
