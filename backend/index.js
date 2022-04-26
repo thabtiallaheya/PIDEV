4a2998ab5454990e39e10d1ce750a280ab8a946c
bouadila <firas.bouadila@esprit.tn>
2022-04-26 23:56:45 +0100

    Fix eya's work

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,7 @@ const passport = require("passport");
 const { update } = require("./models/Activity");
 const fileRoutes = require("./routes/fileUploadRoutes");
 const ACTIVITY = require("./models/Activity");
-const http = require('http');
+const http = require("http");
 const morgan = require("morgan");
 const { default: fetch } = require("node-fetch");
 const jwt = require("jsonwebtoken");
@@ -46,13 +46,13 @@ app.use(express.static("images"));
 app.use("/uploads", express.static(path.join("uploads")));
 app.use("/course", express.static(path.join("course")));
 //socket.io
-const http = require("http").Server(app);
-io = require("socket.io")(http, {
+const httpServer = http.Server(app);
+io = require("socket.io")(httpServer, {
   cors: {
     origin: "http://localhost:3000",
   },
 });
-io.listen(8000);
+io.listen(8080);
 //
 app.set("socketio", io);
 app.use(passport.initialize());
@@ -124,78 +124,79 @@ const server1 = app.listen(process.env.PORT || 8081, function () {
   console.log("App started at port:", port);
 });
 
-
-
 // socket
 
 const socketUtils = require("./utils/socketUtils");
 
 const server2 = http.createServer(app);
-const io = socketUtils.sio(server2);
-socketUtils.connection(io);
+const io1 = socketUtils.sio(server2);
+socketUtils.connection(io1);
 const socketIOMiddleware = (req, res, next) => {
-  req.io = io;
+  req.io = io1;
 
   next();
 };
 
-// CORS
+// // CORS
 app.use(cors());
 
-
-// ROUTES
+// // ROUTES
 app.use("/api/v1/hello", socketIOMiddleware, (req, res) => {
   req.io.emit("message", `Hello, ${req.originalUrl}`);
   res.send("hello world!");
 });
 
-// LISTEN
+// // LISTEN
 
 server2.listen(8002, () => {
   console.log("App running on port 8002...");
 });
 
-
 //web socket
 const webSocketsServerPort = 8000;
-const webSocketServer = require('websocket').server;
+const webSocketServer = require("websocket").server;
 // Spinning the http server and the websocket server.
 const server = http.createServer();
 server.listen(webSocketsServerPort);
-console.log('listening on port 8000');
+console.log("listening on port 8000");
 const wsServer = new webSocketServer({
-  httpServer: server
+  httpServer: server,
 });
 // I'm maintaining all active connections in this object
 const clients = {};
 
 // This code generates unique userid for everyuser.
 const getUniqueID = () => {
-  const s4 = () => Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
-  return s4() + s4() + '-' + s4();
+  const s4 = () =>
+    Math.floor((1 + Math.random()) * 0x10000)
+      .toString(16)
+      .substring(1);
+  return s4() + s4() + "-" + s4();
 };
 
-wsServer.on('request', function(request) {
+wsServer.on("request", function (request) {
   var userID = getUniqueID();
-  console.log((new Date()) + ' Recieved a new connection from origin ' + request.origin + '.');
+  console.log(
+    new Date() +
+      " Recieved a new connection from origin " +
+      request.origin +
+      "."
+  );
   // You can rewrite this part of the code to accept only the requests from allowed origin
   const connection = request.accept(null, request.origin);
   clients[userID] = connection;
-  console.log('connected: ' + userID + ' in ' + Object.getOwnPropertyNames(clients))
-  connection.on('message', function(message) {
-    if (message.type === 'utf8') {
-      console.log('Received Message: ', message.utf8Data);
+  console.log(
+    "connected: " + userID + " in " + Object.getOwnPropertyNames(clients)
+  );
+  connection.on("message", function (message) {
+    if (message.type === "utf8") {
+      console.log("Received Message: ", message.utf8Data);
 
       // broadcasting message to all connected clients
-      for( var key in clients) {
+      for (var key in clients) {
         clients[key].sendUTF(message.utf8Data);
-        console.log('sent Message to: ', clients[key]);
+        console.log("sent Message to: ", clients[key]);
       }
     }
-  })
-
-  
-
-
-
+  });
 });
